23eac475246e2da088c2ee65df3abf2d5d4ecb9e
Nakshtra <nakshtra.jain@venuemonk.com>
2023-08-01 22:21:40 +0530

    changes

---
+++ backend/db/comments.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose')
+
+mongoose.connect('mongodb://0.0.0.0:27017/restaurent')
+
+commentSchema=new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    userId:String,
+    comment:{
+        type:String,
+        required:true
+    }
+
+})
+
+module.exports=mongoose.model("comments",commentSchema)
---
+++ backend/db/users.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose')
+
+mongoose.connect('mongodb://0.0.0.0:27017/restaurent')
+
+userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports=mongoose.model("users",userSchema)
